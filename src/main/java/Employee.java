4afa40058e8716b1b3e029fcb82f4f18f982eb50
Tamia White <tamiawhite@Tamias-MBP.fios-router.home>
2018-11-25 12:43:28 -0500

    Initial commit

---
+++ src/main/java/Employee.java
@@ -0,0 +1,31 @@
+public class Employee {
+
+        private long SSN;
+        private long DOB;
+
+        public long getSSN() {
+            return SSN;
+        }
+
+        public void setSSN(long SSN) {
+            this.SSN = SSN;
+        }
+
+        public long getDOB() {
+            return DOB;
+        }
+
+        public void setDOB(long DOB) {
+            this.DOB = DOB;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        private String name;
+}
